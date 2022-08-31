99c0ba978e48734bd79b42dd6231610353686193
Dylan Lewis <103405022+BubbleManster@users.noreply.github.com>
2022-08-31 11:53:04 +0100

    Add files via upload
    A MicroPython Door Sensor (Reed Switch) Script.

---
+++ Door Sensor/main.py
@@ -0,0 +1,22 @@
+from machine import Pin
+import utime
+led = Pin(25, Pin.OUT)
+buzzer = Pin(22, Pin.OUT)
+sensor = machine.Pin(6, machine.Pin.IN, machine.Pin.PULL_UP)
+exception=False
+def blink():
+    led.toggle()
+    buzzer.toggle()    
+def stop():
+    led.off()
+    buzzer.off()
+while True:
+    val1= sensor.value()
+    utime.sleep(0.01)
+    val2= sensor.value()
+    if val1 and not val2:
+        print("Door Closed")
+    elif not val1 and val2 and exception==False:
+        blink()
+        utime.sleep(2)
+        stop()
